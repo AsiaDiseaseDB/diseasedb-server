26827dd76785dc6382c63d6b7dcc1e22c515d071
赵明宇 <498973030@qq.com>
2018-02-03 16:16:18 +0800

    add a mail receiver

--- routes/handleIssue.js
+++ routes/handleIssue.js
@@ -13,12 +13,39 @@ router.post('/bug', (req, res, next) => {
     }
   })
 
-  // setup email data with unicode symbols
   let mailOptions = {
     from: '"ADD Project" <addproject@163.com>', // sender address
-    to: '498973030@qq.com', // list of receivers
+    to: ['laiyingsi@163.com', '498973030@qq.com'], // list of receivers
     subject: 'Bug Report', // Subject line
     text: bugDescription // plain text body
+  }
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({ success: false, err: error })
+    } else {
+      res.json({ success: true, err: null })
+    }
+  })
+})
+
+router.post('/newuser', (req, res, next) => {
+  let username = req.body.username
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: '163',
+    auth: {
+      user: 'addproject@163.com',
+      pass: 'addproject163163'
+    }
+  })
+
+  let mailOptions = {
+    from: '"ADD Project" <addproject@163.com>', // sender address
+    to: ['laiyingsi@163.com', '498973030@qq.com'], // list of receivers
+    subject: 'New User Registered', // Subject line
+    text: username + ' has joined the online database system!' // plain text body
   }
 
   // send mail with defined transport object
